3904445ac2d6bc4727a2ac318c9a7f045ec569e3
psh0718 <psh970718@naver.com>
2016-09-25 17:55:06 +0900

    문제 2-4

---
+++ Person.java
@@ -0,0 +1,12 @@
+﻿class Person {
+    private float weight;
+    public void setWeight(float w) {
+        this.weight=w;
+    }
+    private float getWeight() {
+        return weight;
+    }
+}
+Person me=new Person();
+me.setWeight((float)70.0);
+System.out.println(me.getWeight());

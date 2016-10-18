4b95ef958b6f7fb20c349e3b7356ca7d0beafcb9
psh0718 <psh970718@naver.com>
2016-10-19 03:40:32 +0900

    문제 4-1 도형그리기

---
+++ TurtleDraw.java
@@ -0,0 +1,37 @@
+import ch.aplu.turtle.*;
+
+Turtle t1 = new Turtle();
+class TurtleDraw{
+	void drawTriangle(int size, double x, double y){
+	    t1.up();
+	    t1.goto(x,y);
+	    t1.down();
+	    for(int i=0; i<3; i++){
+	        t1.fd(size);
+	        t1.right(120);
+	    }
+	}
+	void drawSquare(int size, double x, double y){
+	    t1.up();
+	    t1.goto(x,y);
+	    t1.down();
+	    for(int i=0; i<4; i++){
+	        t1.fd(size);
+	        t1.right(90);
+	    }
+	}
+	void drawStar(int size, double x, double y){
+	    t1.up();
+	    t1.goto(x,y);
+	    t1.down();
+	    for(int i=0; i<5; i++){
+	        t1.fd(size);
+	        t1.right(72);
+	    }
+	}
+}
+
+TurtleDraw f = new TurtleDraw();
+f.drawTriangle(70, -30.0 , 0.0);
+f.drawSquare(60, 0.0 , 0.0);
+f.drawStar(50, 30.0 , 0.0);

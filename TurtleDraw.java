d123e594354e06b86bb39f0b13acadb35e64a682
201611072 박승훈 / 과제 위치 - 201611072_j <psh970718@naver.com>
2016-10-31 15:52:40 +0900

    제어-1

--- TurtleDraw.java
+++ TurtleDraw.java
@@ -3,27 +3,27 @@ import ch.aplu.turtle.*;
 Turtle t1 = new Turtle();
 class TurtleDraw{
     void drawTriangle(int size, double x, double y){
-        t1.up();
+        t1.penUp();
         t1.setPos(x,y);
-        t1.down();
+        t1.penDown();
         for(int i=0; i<3; i++){
             t1.fd(size);
             t1.right(120);
         }
     }
     void drawSquare(int size, double x, double y){
-        t1.up();
+        t1.penUp();
         t1.setPos(x,y);
-        t1.down();
+        t1.penDown();
         for(int i=0; i<4; i++){
             t1.fd(size);
             t1.right(90);
         }
     }
     void drawStar(int size, double x, double y){
-        t1.up();
+        t1.penUp();
         t1.setPos(x,y);
-        t1.down();
+        t1.penDown();
         for(int i=0; i<5; i++){
             t1.fd(size);
             t1.right(144);
